3df2175d990dd3c6190218343252d6fc24f36536
jreyes <justin.reyes.145@my.csun.edu>
2023-04-18 14:16:32 -0700

    Removed "object sender" from ShapeTool

--- Pixel Forgery/Pixel Forgery/ShapeTool.cs
+++ Pixel Forgery/Pixel Forgery/ShapeTool.cs
@@ -29,10 +29,9 @@ namespace Pixel_Forgery
         /// Contains a switch that checks the typeOfTool and calls the appropriate draw function.
         /// Draws to the pictureBox with Graphics g.
         /// </summary>
-        /// <param name="sender">is the object type of the control which sent the command</param>
         /// <param name="e">is the mouse event parameter and contains data like location and mouse button click type</param>
         /// <param name="pictureBox1">is the actual picturebox from Form1 sent to the tool so new graphics will be saved</param>
-        public override void useTool(object sender, MouseEventArgs e, System.Windows.Forms.PictureBox pictureBox1)
+        public override void useTool(MouseEventArgs e, System.Windows.Forms.PictureBox pictureBox1)
         {
             Graphics g = Graphics.FromImage(pictureBox1.Image);
             p.Color = currentColor;
@@ -61,9 +60,8 @@ namespace Pixel_Forgery
         /// Contains a switch to check and draw the appropriate shape based on type of tool.
         /// Draws with the passed Graphics g from onPaint in Form1.
         /// </summary>
-        /// <param name="sender">is the object type of the control which sent the command</param>
         /// <param name="e">is the paint event handler and contains the graphics of the control</param>
-        public override void drawOutline(object sender, PaintEventArgs e)
+        public override void drawOutline(PaintEventArgs e)
         {
             Graphics g = e.Graphics;
             switch (typeOfTool)
@@ -90,7 +88,6 @@ namespace Pixel_Forgery
         /// assigns the passed list to the tool object's list
         /// (previous work got wiped out by an accidental revert during a sync, polygon draw will be operational this weekend)
         /// </summary>
-        /// <param name="sender">is the object type of the control which sent the command</param>
         /// <param name="points">is the list of points used to draw polygons</param>
         public override void points(List<Point> points)
         {
