643741ee2683e7cd529f8a5aa6e2bfe9eba2d1c4
lrros <lrrosales@outlook.com>
2023-04-18 18:48:12 -0700

    Complete documentation for BrushTools

--- Pixel Forgery/Pixel Forgery/BrushTool.cs
+++ Pixel Forgery/Pixel Forgery/BrushTool.cs
@@ -9,6 +9,15 @@ using static System.Windows.Forms.AxHost;
 
 namespace Pixel_Forgery
 {
+    /// <summary>
+    /// Date: 3/13/23
+    /// Programmer(s): Justin Reyes & Lilianna Rosales
+    /// BrushTool class controls the construction and use of the brush
+    /// Contains 1 method:
+    /// useTool() which creates brush strokes by by changing color the pixels from pictureBox1 
+    /// Contains 1 property variable:
+    /// BrushWidth which gets and sets the brush pen width
+    /// </summary>
     public class BrushTool : PixelForgeryTool
     {
         // Create the pen object only once. This makes it easier to 
@@ -16,6 +25,8 @@ namespace Pixel_Forgery
         private Pen p = new Pen(Color.Black, 5);
 
         /// <summary>
+        /// Date: 4/05/23
+        /// Programmer(s): Lilianna Rosales
         /// Property variable for setting and getting the 
         /// brush pen width
         /// </summary>
@@ -25,6 +36,13 @@ namespace Pixel_Forgery
             get { return p.Width; }
         }
 
+        /// <summary>
+        /// Date: 3/13/23
+        /// Programmer(s): Justin Reyes
+        /// Creates brush strokes by by changing color the pixels from pictureBox1
+        /// </summary>
+        /// <param name="e">is the mouse event parameter and contains data like location and mouse button click type</param>
+        /// <param name="pictureBox1">Reference to the pictureBox.</param>
         public override void useTool(MouseEventArgs e, System.Windows.Forms.PictureBox pictureBox1)
         {
             Graphics g = Graphics.FromImage(pictureBox1.Image);
