a70d3dc956ec5a4b9c2e775e3d4929a12baa6f3a
lrros <lrrosales@outlook.com>
2023-04-18 18:27:59 -0700

    partial Tools documentation

--- Pixel Forgery/Pixel Forgery/Tool.cs
+++ Pixel Forgery/Pixel Forgery/Tool.cs
@@ -7,6 +7,15 @@ using System.Windows.Forms;
 
 namespace Pixel_Forgery
 {
+    /// <summary>
+    /// Date: 3/13/23
+    /// Programmer(s): Justin Reyes, Gregory Khrom-Abramyan, Taylor Nastally, Lilianna Rosales
+    /// Provides basic functionality needed for the use of tools withing Pixel_Forgery 
+    /// Contains 3 method:
+    /// useTool() overritten by BrushTool, EraserTool, FillTool
+    /// drawOutline() overritten by ShapeTool
+    /// points() overritten by ShapeTool
+    /// </summary>
     public abstract class PixelForgeryTool
     {
         public int startX;
@@ -17,6 +26,12 @@ namespace Pixel_Forgery
         public Boolean isDrawing = false;
         public Pen p = new Pen(Color.Black, 5);
 
+        /// <summary>
+        /// Date: 4/05/23
+        /// Programmer(s): Lilianna Rosales
+        /// Property variable for setting and getting the 
+        /// current pen color
+        /// </summary>
         public Color currentColor 
         {
             set { p.Color = value; }
