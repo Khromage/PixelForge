ddd726fc31cca228908adff5935a4895891dff2e
Khromage <khromage@gmail.com>
2023-05-05 20:12:51 -0700

    Compare Bitmap test method

--- Pixel Forgery/PixelForgeTest/UnitTest.cs
+++ Pixel Forgery/PixelForgeTest/UnitTest.cs
@@ -49,5 +49,38 @@ namespace PixelForgeTest
             //Testing image cancel
             Assert.AreEqual(bmpActual, bmpTest, "Test Passed!");
         }
+
+
+        [TestMethod]
+        public void TestBitmapCompare()
+        {//open image, open new image
+            FileExplorerDialog dialog = new FileExplorerDialog();
+            Bitmap bmpTest = new Bitmap(1, 1);
+            //bmpTest = dialog.OpenFile(bmpTest);
+
+            var imageFile = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            Image image = Image.FromFile(Path.Combine(imageFile, "PixelForgeLogo_Final.png"));
+            Bitmap bmpActual = new Bitmap(image);
+
+            bmpTest = dialog.OpenFile(bmpActual);
+            //if press cancel, bmpTest == actual
+            //else is different, !=
+            //if same image == 
+
+            bool mismatch = false;
+            for (int x = 0; x < bmpActual.Width; x++)
+            {
+                for (int y = 0; y < bmpActual.Height; y++)
+                {
+                    if (bmpActual.GetPixel(x, y).ToArgb() != bmpTest.GetPixel(x, y).ToArgb())
+                    {
+                        mismatch = true;
+                        break;
+                    }
+                }
+                if (mismatch) break;
+            }
+            Assert.IsFalse(mismatch);
+        }
     }
 }
