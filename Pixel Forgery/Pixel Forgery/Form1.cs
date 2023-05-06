0adeb4e2e02bf8e00d0310c9ef1d543400b1b12a
Khromage <khromage@gmail.com>
2023-05-05 17:47:36 -0700

    ..

--- Pixel Forgery/Pixel Forgery/Form1.cs
+++ Pixel Forgery/Pixel Forgery/Form1.cs
@@ -27,6 +27,8 @@ namespace Pixel_Forgery
         private TextTool textTool = new TextTool();
         private FillTool fillTool = new FillTool();
         private List<Point> points = new List<Point>();
+        private int scaleFactor = 15;
+        private float constant = 1.7f;
 
         /// <summary>
         /// Constructor for the PixelForgeryGUI class.
@@ -59,7 +61,7 @@ namespace Pixel_Forgery
         }
 
         /// <summary>
-        /// Creates a new canvas for the user to draw in, defaults to 918x595 pixels in size.
+        /// Creates a new canvas for the user to draw in, defaults to 1280x720 pixels in size.
         /// <list type="bullet">
         /// <item>Date: 4/16/23</item>
         /// <item>Programmer(s): Justin Reyes</item>
@@ -69,7 +71,7 @@ namespace Pixel_Forgery
         /// <param name="e">An EventListener checking for when the Button is clicked</param>
         private void NewButton_Click(object sender, EventArgs e)                // New Button
         {
-            BMP = new Bitmap(918, 595);
+            BMP = new Bitmap(1280, 720);
             using (Graphics g = Graphics.FromImage(BMP))
             {
                 Rectangle bg = new Rectangle(0, 0, BMP.Width, BMP.Height);
@@ -765,6 +767,18 @@ namespace Pixel_Forgery
                 pictureBox.Cursor = new Cursor(cursorBMP.GetHicon());
                 pictureBox.Cursor.Draw(g, r);
             }
+        }
+
+        private void zoomInButton_Click(object sender, EventArgs e)
+        {
+            pictureBox.Height += Convert.ToInt32(scaleFactor / constant);
+            pictureBox.Width += scaleFactor;
+        }
+
+        private void zoomOutButton_Click(object sender, EventArgs e)
+        {
+            pictureBox.Height -= Convert.ToInt32(scaleFactor / constant);
+            pictureBox.Width -= scaleFactor;
         }
 
         /// <summary>
