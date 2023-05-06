400e3a072d55bef2662891ac2a3e9e6109b5c2db
jreyes <justin.reyes.145@my.csun.edu>
2023-05-05 20:17:02 -0700

    Fill Tool test

--- Pixel Forgery/PixelForgeTest/UnitTest.cs
+++ Pixel Forgery/PixelForgeTest/UnitTest.cs
@@ -50,6 +50,24 @@ namespace PixelForgeTest
             Assert.AreEqual(bmpActual, bmpTest, "Test Passed!");
         }
 
+        [TestMethod]
+        public void DfsFillTest()
+        {
+            FillTool f = new FillTool();
+            FileExplorerDialog dialog = new FileExplorerDialog();
+            Bitmap bmpTest = new Bitmap(1, 1);
+
+            f.p.Color = Color.Blue;
+
+            Bitmap bmp = new Bitmap(100, 100);
+            bmp = f.DfsFill(50, 50, bmp);
+
+            var imageFile = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            Image image = Image.FromFile(Path.Combine(imageFile, "Testing2.png"));
+            Bitmap bmpActual = new Bitmap(image);
+
+            bmp.Save(Path.Combine(imageFile, "Testing2.png"));
+        }
 
         [TestMethod]
         public void TestBitmapCompare()
